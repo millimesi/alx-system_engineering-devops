d333d250b581f3b6b13daac3b4ad22f687207e00
millimesi <millionmesi1@gmail.com>
2024-02-24 19:15:18 +0300

    hard

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,26 @@
-# create a file in /tmp
-file {'~/.ssh/config':
-    ensure  => file,
-    content => "
-        Host *
-            IdentityFile ~/.ssh/school
-            PasswordAuthentication no
-    "
+# create a file with puppet
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',  
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',  
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
+
+file_line { 'Set up SSH client for my_server':
+  path  => '/etc/ssh/ssh_config',  
+  line  => 'HostName 100.25.177.26',
+  match => '^#?HostName',
+}
+
+file_line { 'Set up SSH client user for my_server':
+  path  => '/etc/ssh/ssh_config',  
+  line  => 'User ubuntu',
+  match => '^#?User',
 }
