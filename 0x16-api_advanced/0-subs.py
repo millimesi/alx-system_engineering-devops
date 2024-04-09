767539dc9d9b2d845dd77a5b1eafe2270cc9e579
millimesi <millionmesi1@gmail.com>
2024-04-09 15:30:18 +0300

    some requirements

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,17 +12,10 @@ def number_of_subscribers(subreddit):
     '''
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Reddit API v1.0"}
-
-    try:
-        response = requests.get(
-            api_url, allow_redirects=False, headers=headers
-            )
-        response.raise_for_status()
-
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        if "data" in data and "subscribers" in data["data"]:
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    except requests.exceptions.RequestException:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
