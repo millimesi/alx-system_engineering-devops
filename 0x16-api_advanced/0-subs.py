aac6be69e6abd34c0078f3319487af4188e2d0b9
millimesi <millionmesi1@gmail.com>
2024-04-09 15:32:58 +0300

    some requirementsss

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-'''
-Api advanced
-'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-    returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit
-    '''
-    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Reddit API v1.0"}
-
-    try:
-        response = requests.get(
-            api_url, allow_redirects=False, headers=headers
-            )
-        response.raise_for_status()
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        if "data" in data and "subscribers" in data["data"]:
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    except requests.exceptions.RequestException:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
